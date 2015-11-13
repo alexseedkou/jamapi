eda9a3610fa615bd0652019158cb55a05dd9dbf5
Song Liao <liaosong1991@hotmail.com>
2015-11-13 15:01:43 -0600

    add chords preview

--- app/models/tabs_set.rb
+++ app/models/tabs_set.rb
@@ -2,6 +2,20 @@ class TabsSet < ActiveRecord::Base
   belongs_to :song
   belongs_to :user
 
+  def chords_preview
+    #get first 8 chords of the song and concatenate to a string
+    preview = ""
+    unless chords.nil?
+      for i in 0..chords.count-1
+        if i >= 8
+          break
+        end
+          preview += chords[i] + "  "
+      end
+    end
+    return preview
+  end
+
   def average_votes
     return upvotes - downvotes
   end
--- app/serializers/tabs_set_serializer.rb
+++ app/serializers/tabs_set_serializer.rb
@@ -1,3 +1,3 @@
 class TabsSetSerializer < ActiveModel::Serializer
-  attributes :id, :tuning, :capo, :upvotes, :downvotes, :song_id, :user_id
+  attributes :id, :tuning, :capo, :upvotes, :downvotes, :song_id, :user_id, :chords_preview
 end
