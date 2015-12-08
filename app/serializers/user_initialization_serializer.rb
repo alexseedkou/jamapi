2351c1ff9da5b4f93d142545c26815ecd389c74f
Song Liao <liaosong1991@hotmail.com>
2015-12-08 14:10:40 -0600

    add user nickname and avatar when initialize a user

--- app/serializers/user_initialization_serializer.rb
+++ app/serializers/user_initialization_serializer.rb
@@ -1,3 +1,3 @@
 class UserInitializationSerializer < ActiveModel::Serializer
-  attributes :id, :email, :auth_token
+  attributes :id, :email, :auth_token, :nickname, :avatar_url_medium, :avatar_url_thumbnail
 end
