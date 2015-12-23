b5add1b6e5fcbe98367dad8a7797e90c1f2ede45
Song Liao <liaosong1991@hotmail.com>
2015-12-23 13:20:38 -0600

    add preview to setContent serializer anyway

--- app/serializers/lyrics_set_content_serializer.rb
+++ app/serializers/lyrics_set_content_serializer.rb
@@ -1,3 +1,4 @@
 class LyricsSetContentSerializer < ActiveModel::Serializer
-  attributes :id, :cached_votes_score, :times, :lyrics, :song_id, :user_id
+  attributes :id, :cached_votes_score, :times, :lyrics, :song_id, :user_id, :number_of_lines,
+   :lyrics_preview
 end
--- app/serializers/tabs_set_content_serializer.rb
+++ app/serializers/tabs_set_content_serializer.rb
@@ -1,3 +1,3 @@
 class TabsSetContentSerializer < ActiveModel::Serializer
-  attributes :id, :cached_votes_score, :tuning, :capo, :times, :chords, :tabs, :song_id, :user_id
+  attributes :id, :cached_votes_score, :tuning, :capo, :times, :chords, :tabs, :song_id, :user_id, :chords_preview
 end
