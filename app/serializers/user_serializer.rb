6bd200551c800d72a189e02b7b404efeca2f8b0e
Song Liao <liaosong1991@hotmail.com>
2015-12-22 21:06:12 -0600

    return full tabsSet and lyricsSet upon each show user calling

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :nickname, :avatar_url_medium, :avatar_url_thumbnail
-  has_many :tabs_sets
-  has_many :lyrics_sets
+  has_many :tabs_sets, serializer: TabsSetContentSerializer
+  has_many :lyrics_sets, serializer: LyricsSetContentSerializer
 end
