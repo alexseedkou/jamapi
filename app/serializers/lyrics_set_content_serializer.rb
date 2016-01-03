5c28b06d5c51bfa7d210062ccde74c9a1a1b8c73
Song Liao <liaosong1991@hotmail.com>
2016-01-03 17:34:05 -0600

    add last_edited to set content

--- app/serializers/lyrics_set_content_serializer.rb
+++ app/serializers/lyrics_set_content_serializer.rb
@@ -1,5 +1,5 @@
 class LyricsSetContentSerializer < ActiveModel::Serializer
   attributes :id, :cached_votes_score, :times, :lyrics, :song_id, :user_id, :number_of_lines,
-   :lyrics_preview, :visible
+   :lyrics_preview, :visible, :last_edited
    has_one :song, serializer: SongInformationSerializer
 end
--- app/serializers/tabs_set_content_serializer.rb
+++ app/serializers/tabs_set_content_serializer.rb
@@ -1,5 +1,5 @@
 class TabsSetContentSerializer < ActiveModel::Serializer
   attributes :id, :cached_votes_score, :tuning, :capo, :times, :chords, :tabs,
-  :song_id, :user_id, :chords_preview, :visible
+  :song_id, :user_id, :chords_preview, :visible, :last_edited
   has_one :song, serializer: SongInformationSerializer
 end
