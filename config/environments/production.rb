42678d18366bf3acfe9baa1837462a3929bbefdc
Song Liao <liaosong1991@hotmail.com>
2016-01-20 22:20:55 -0600

    fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,20 +47,18 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-  config.assets.raise_runtime_errors = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'https://jamapi.herokuapps.com',
+    domain:               'https://jamapi.herokuapp.com',
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true
-   }
-   config.action_controller.default_url_options = { host: 'https://jamapi.herokuapps.com' }
-   config.action_mailer.default_url_options = { host: 'https://jamapi.herokuapps.com' }
+  }
+   config.action_controller.default_url_options = { host: 'https://jamapi.herokuapp.com' }
+   config.action_mailer.default_url_options = { host: 'https://jamapi.herokuapp.com' }
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
