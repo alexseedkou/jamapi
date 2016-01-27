0df1531a7d552ed2bcee43ca0497eb086ac08fc6
Song Liao <liaosong1991@hotmail.com>
2016-01-27 16:04:53 -0600

    show aliases

--- app/serializers/song_information_serializer.rb
+++ app/serializers/song_information_serializer.rb
@@ -1,4 +1,4 @@
 class SongInformationSerializer < ActiveModel::Serializer
   attributes :id, :title, :artist, :duration, :total_score, :soundwave_url, :in_iTunes,
-  :track_id, :album, :artwork_url, :preview_url, :store_link
+  :track_id, :album, :artwork_url, :preview_url, :store_link, :title_aliases, :artist_aliases
 end
