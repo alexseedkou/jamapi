f0514a3ac59a01bff6800bac9760c5279b93362e
Song Liao <liaosong1991@hotmail.com>
2016-02-02 10:21:39 -0600

    add set score for testing

--- app/serializers/song_information_serializer.rb
+++ app/serializers/song_information_serializer.rb
@@ -1,4 +1,4 @@
 class SongInformationSerializer < ActiveModel::Serializer
-  attributes :id, :title, :artist, :duration, :total_score, :soundwave_url, :in_iTunes,
+  attributes :id, :title, :artist, :duration, :total_score, :set_scores, :soundwave_url, :in_iTunes,
   :track_id, :album, :artwork_url, :preview_url, :store_link, :title_aliases, :artist_aliases
 end
